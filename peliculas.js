54fca8ba36861cc540923c7c491c6bf142bdf1d2
Nicole Colmenarez <nicolecolmenares412@gmail.com>
2023-09-20 12:43:10 -0300

    Agregar Peliculas al conflicto

--- peliculas.js
+++ peliculas.js
@@ -1,5 +1,5 @@
 let peliculasMarvel = ['Capitan America', 'Iron Man', 'Hulk'];
 let peliculasDC =['Mujer Maravilla', 'Batman']
-let pelculasMiedo =['La Monja', 'El Conjuro', 'Annabelle']
+let pelculasMiedo =['La Monja', 'El Conjuro', 'Annabelle', 'chucky']
 
 console.log(peliculasDC, peliculasMarvel);
