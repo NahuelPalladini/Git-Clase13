0da2a0014c57729304ba97f36b26e2db70abd74c
Nicole Colmenarez <nicolecolmenares412@gmail.com>
2023-09-20 12:51:03 -0300

    solucionando los conflictos

--- peliculas.js
+++ peliculas.js
@@ -1,5 +1,5 @@
 let peliculasMarvel = ['Capitan America', 'Iron Man', 'Hulk'];
-let peliculasDC =['Mujer Maravilla', 'Batman']
+let peliculasDC =['Mujer Maravilla', 'Batman', 'Super Man']
 let pelculasMiedo =['La Monja', 'El Conjuro', 'Annabelle', 'chucky']
 
 console.log(peliculasDC, peliculasMarvel);
