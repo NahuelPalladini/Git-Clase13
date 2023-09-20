c18cb3c9b3daac23ee8ec4f115ea8d61b89e9b3b
Nicole Colmenarez <nicolecolmenares412@gmail.com>
2023-09-20 12:39:27 -0300

    Agregar Peliculas

--- peliculas.js
+++ peliculas.js
@@ -1,4 +1,5 @@
 let peliculasMarvel = ['Capitan America', 'Iron Man', 'Hulk'];
 let peliculasDC =['Mujer Maravilla', 'Batman']
+let pelculasMiedo =['La Monja', 'El Conjuro', 'Annabelle']
 
 console.log(peliculasDC, peliculasMarvel);
