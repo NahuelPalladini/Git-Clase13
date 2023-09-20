a1e2f705d0e0f559d2c1bd29725f3e9f6e62363a
NahuelPalladini <palladininahuel@gmail.com>
2023-09-20 13:01:14 -0300

    Conflicto Resuelto

--- peliculas.js
+++ peliculas.js
@@ -1,7 +1,6 @@
 let peliculasMarvel = ['Capitan America', 'Iron Man', 'Hulk'];
 let peliculasDC =['Mujer Maravilla', 'Batman'];
 let pelculasMiedo =['La Monja', 'El Conjuro', 'Annabelle', 'chucky', 'Actividad Paranoramal'];
-let peliculasDC =['Mujer Maravilla', 'Batman', 'Super Man']
-let pelculasMiedo =['La Monja', 'El Conjuro', 'Annabelle', 'chucky']
 
-console.log(peliculasDC, peliculasMarvel);
+
+console.log(peliculasDC, peliculasMarvel, pelculasMiedo);
